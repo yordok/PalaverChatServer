54c1d12b40126c9e08c6cc33c12530c618b3e0ba
Yorden <jmk1637@rit.edu>
2015-12-03 07:13:37 -0500

    added account creation socket handler

--- src/server/server.onSocketLogin.js
+++ src/server/server.onSocketLogin.js
@@ -3,7 +3,7 @@ var roomHandler = require('../utilities/roomHandler.js');
 var userModel = require("../models/User.js");
 
 var onSocketLogin = function(socket){
-
+  //login
   socket.on("login", function(data){
     userModel.userModel.authenticate(data.username, data.password, function(error, account){
       if(error){
@@ -14,11 +14,76 @@ var onSocketLogin = function(socket){
       }
       else{
         var AccountInfo = account.toAPI();
+        socket.UserID = data.username;
         socket.emit("LoginSuccessful", AccountInfo);
         console.log(AccountInfo);
       }
     });
   });
+  //changePreferences
+  socket.on("changePreferences",function(data){
+    //var response = userModel.userModel.changeData(newData);
+    userModel.userModel.findOne({ username: socket.UserID }, function(error, user){
+      if(error){
+          console.log("error with finding");
+          socket.emit("changePreferencesError");
+      }
+      else if(user == null){
+          console.log("error with finding user");
+          socket.emit("changePreferencesError");
+
+      }
+      else{
+          user.wantsCustomName = data.wantsCustomName;
+          user.wantsCustomColor = data.wantsCustomColor;
+          user.customName = data.CustomName;
+          user.customColor = data.CustomColor;
+          user.save(function(error, data){
+              if(error){
+                console.log("error with saving");
+                socket.emit("changePreferencesError");
+              }
+              else{
+                socket.emit("changePreferencesSuccess");
+                //res.redirect('/success');
+              }
+          });
+      }
+    });
+  });
+
+
+  socket.on("createAccount",function(data){
+    userModel.userModel.generateHash(data.pass1, function (salt, hash){
+      var userData = {
+        username:data.username,
+        salt: new Buffer(salt),
+        password: hash
+      };
+
+      var newUserModel = new userModel.userModel(userData);
+
+      newUserModel.save(function(err){
+        if(err){
+          console.log(err);
+          if(err.code == 11000){
+            socket.emit("accountCreationError");
+
+          }
+
+          socket.emit("accountCreationError");
+
+        }
+        else{
+          socket.emit("accountCreationSuccess");
+
+        }
+
+      });
+
+    });
+  });
+
 };
 
 
